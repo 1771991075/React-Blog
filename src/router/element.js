3897c44963739973b9a5e96a59d0dbf78c73d9c3
lzc <lvzicong123@outlook.com>
2023-04-07 20:41:53 +0800

    配置路由表

--- src/router/element.js
+++ src/router/element.js
@@ -1,28 +1,63 @@
 import {Navigate} from 'react-router-dom';
 import {lazy,Suspense} from 'react';
-import Home from '../views/Home';
 import Index from '../views/Index';
-import Blog from '../views/Blog';
+import Loading from "../component/Loading";
+const Home = lazy(()=>import('../views/Home'));
+const Blog = lazy(()=>import('../views/Blog'));
+const Cate = lazy(()=>import('../views/Cate'));
+const About = lazy(()=>import('../views/About'));
+const Feedback = lazy(()=>import('../views/Feedback'));
+const Login = lazy(()=>import('../views/Login'));
+const NotFound = lazy(()=>import('../component/NotFound'));
 
 let element = [
     {
         path:'/index',
         element:<Index/>,
+        author: false,
         children:[
             {
                 path:'home',
-                element:<Home/>,
+                element:<Suspense fallback={<Loading/>}><Home /></Suspense>,
+                author: false
             },
             {
                 path:'blog',
-                element:<Blog/>,
+                element:<Suspense fallback={<Loading/>}><Blog /></Suspense>,
+                author: false
+            },
+            {
+                path:'cate',
+                element:<Suspense fallback={<Loading/>}><Cate /></Suspense>,
+                author: false
+            },
+            {
+                path:'about',
+                element:<Suspense fallback={<Loading/>}><About /></Suspense>,
+                author: false
+            },
+            {
+                path:'feedback',
+                element:<Suspense fallback={<Loading/>}><Feedback /></Suspense>,
+                author: false
             },
         ]
     },
     {
+        path:'/login',
+        element:<Suspense fallback={<Loading/>}><Login /></Suspense>,
+        author: false
+    },
+    {
         path:'/',
-        element:<Navigate to='/index/home'></Navigate>
-    }
+        element:<Navigate to='/index/home'></Navigate>,
+        author: false
+    },
+    {
+        path: '*',
+        element: <Suspense fallback={<Loading/>}><NotFound /></Suspense>,
+        author: false
+    },
 ]
 
 export default element;
