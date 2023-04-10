78a6e1d77ee3522603c937dba918ca8513f74f31
lzc <lvzicong123@outlook.com>
2023-04-10 17:05:56 +0800

    about

--- src/component/About/ArtStack/index.jsx
+++ src/component/About/ArtStack/index.jsx
@@ -3,26 +3,31 @@ import react from '../../../assets/img/reactbg.jpg';
 import redux from '../../../assets/img/reduxbg.png';
 import nodejs from '../../../assets/img/nodejsbg.png';
 import echarts from '../../../assets/img/echartsbg.png';
+import './index.css';
 const gridStyle = {
   width: '25%',
-  padding:'0px',
-  margin:'0px',
+  padding: '0px',
+  margin: '0px',
   cursor: 'pointer'
 };
 const imgbg = {
-  width:'100%',
-  height:'100%',
+  width: '100%',
+  height: '100%',
 }
-const ArtStack = () => (
-  <Card>
-    <Card.Grid style={gridStyle}><img style={imgbg} src={react} alt='react'/></Card.Grid>
-    <Card.Grid style={gridStyle}><img style={imgbg} src={redux} alt='redux'/></Card.Grid>
-    <Card.Grid style={gridStyle}><img style={imgbg} src={nodejs} alt='nodejs'/></Card.Grid>
-    <Card.Grid style={gridStyle}><img style={imgbg} src={echarts} alt='echarts'/></Card.Grid>
-    <Card.Grid style={gridStyle}>JavaScript</Card.Grid>
-    <Card.Grid style={gridStyle}>TypeScript</Card.Grid>
-    <Card.Grid style={gridStyle}>React-Router</Card.Grid>
-    <Card.Grid style={gridStyle}>Nprogress</Card.Grid>
-  </Card>
-);
-export default ArtStack;
+
+export default function ArtStack() {
+  return (
+    <div>
+      <Card>
+        <Card.Grid style={gridStyle} className='aboutbg'><img style={imgbg} src={react} alt='react' /></Card.Grid>
+        <Card.Grid style={gridStyle} className='aboutbg'><img style={imgbg} src={redux} alt='redux' /></Card.Grid>
+        <Card.Grid style={gridStyle} className='aboutbg'><img style={imgbg} src={nodejs} alt='nodejs' /></Card.Grid>
+        <Card.Grid style={gridStyle} className='aboutbg'><img style={imgbg} src={echarts} alt='echarts' /></Card.Grid>
+        {/* <Card.Grid style={gridStyle} className='aboutbg'>JavaScript</Card.Grid>
+        <Card.Grid style={gridStyle} className='aboutbg'>TypeScript</Card.Grid>
+        <Card.Grid style={gridStyle} className='aboutbg'>React-Router</Card.Grid>
+        <Card.Grid style={gridStyle} className='aboutbg'>Nprogress</Card.Grid> */}
+      </Card>
+    </div>
+  )
+}
