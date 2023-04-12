843c58098cce11e763fdeb66f5d427bf95bb545d
lzc <lvzicong123@outlook.com>
2023-04-12 19:59:24 +0800

    博客列表分类

--- src/views/AdminBlogList/index.jsx
+++ src/views/AdminBlogList/index.jsx
@@ -5,8 +5,14 @@ import { ExclamationCircleFilled } from '@ant-design/icons';
 const { confirm } = Modal;
 export default function AdminBlogList() {
     let [blogList, setBlogList] = useState([])
+    //当前展示的博客列表
+    let [nowBlogList,setNowBlogList] = useState([])
     //当前选中的博客id
     let [blogId, setBlogId] = useState('')
+    //当前页数
+    let [defaultCurrent,setDefaultCurrent] = useState(1)
+    //当前每页条数
+    let [defaultPageSize,setDefaultPageSize] = useState(5)
     const columns = [
         {
             title: '标题',
@@ -80,38 +86,41 @@ export default function AdminBlogList() {
         });
     };
     //切换分页
-    const onChange = (pageNumber) => {
-        console.log('Page: ', pageNumber);
+    const onChange = (page, pageSize) => {
+        setDefaultCurrent(page)
+        setDefaultPageSize(pageSize)
+        getblog(page,pageSize)
     };
     //删除博客
     const delBlog = async (id) => {
         let res = await deleteBlog({id})
         if(res.data.code === 200){
             message.success(res.data.msg)
-            getblog()
+            getblog(defaultCurrent,defaultPageSize)
         }else{
             message.error(res.data.msg)
         }
     }
-    const getblog = async () => {
+    const getblog = async (page,PageSize) => {
         let res = await getBlogList()
         console.log(res);
         res.data.data.bloglist.forEach((item, index) => {
             item.tag = item.tag.split(',')
         });
         setBlogList(res.data.data.bloglist)
+        setNowBlogList(res.data.data.bloglist.splice(((page-1)*defaultPageSize),PageSize))
     }
 
     useEffect(() => {
-        getblog()
+        getblog(defaultCurrent,defaultPageSize)
     }, [])
 
     return (
         <div>
             {
-                blogList.length !== 0 && <Table columns={columns} dataSource={blogList} bordered={true} pagination={false} rowKey='id' />
+                setNowBlogList.length !== 0 && <Table columns={columns} dataSource={nowBlogList} bordered={true} pagination={false} rowKey='id' />
             }
-            <Pagination showQuickJumper defaultCurrent={1} total={blogList.length} pageSizeOptions={[5, 10, 20]} onChange={onChange} showSizeChanger style={{ marginTop: '20px' }} />
+            <Pagination showQuickJumper current={defaultCurrent} defaultPageSize={defaultPageSize} PageSize={defaultPageSize} total={blogList.length} pageSizeOptions={[5, 10, 20]} onChange={onChange} showSizeChanger style={{ marginTop: '20px' }} />
         </div>
     )
 }
